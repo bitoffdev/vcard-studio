5dba85fd9114df14908d4eab01556c5eb917d2d7
chronos <chronos@b2f690cb-8b89-471c-b043-dbd4632e1e77>
2021-08-19 13:10:53 +0000

    * Modified: Changes needed after version 1.0.0 release.

--- Install/win/build.bat
+++ Install/win/build.bat
@@ -8,5 +8,5 @@ copy ..\..\%APPNAME%.exe ..\..\lib\i386-win32-Release
 %LAZDIR%\lazbuild.exe --lazarusdir=%LAZDIR% --build-mode="Release" --cpu=x86_64 --operating-system=Win64 ..\..\%APPNAME%.lpi
 copy ..\..\%APPNAME%.exe ..\..\lib\x86_64-win64-Release
 
-"c:\Program Files (x86)\Inno Setup 5\ISCC.exe" "vCard Studio.iss"
+"c:\Program Files (x86)\Inno Setup 6\ISCC.exe" "vCard Studio.iss"
 
