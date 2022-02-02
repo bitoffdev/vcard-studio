574f7ad77b303b5959371ef6fcf1b557a8b20687
Chronos <robie@centrum.cz>
2022-02-02 15:33:25 +0000

    * Added: A windows for showing log output for selected test case. * Added: Various load-save tests. * Modified: Improved parsing vCard format.

--- UTest.pas
+++ UTest.pas
@@ -13,18 +13,73 @@ type
   { TTestCase }
 
   TTestCase = class
+  public
     Name: string;
     Result: TTestResult;
-    procedure Run;
+    Log: string;
+    procedure Run; virtual;
   end;
+
+  TTestCaseClass = class of TTestCase;
 
   { TTestCases }
 
   TTestCases = class(TFPGObjectList<TTestCase>)
-    function AddNew(Name: string): TTestCase;
+    function AddNew(Name: string; TestClass: TTestCaseClass): TTestCase;
   end;
 
+  { TTestCaseLoadSave }
+
+  TTestCaseLoadSave = class(TTestCase)
+    Input: string;
+    Output: string;
+    procedure Run; override;
+    procedure Evaluate(Passed: Boolean);
+  end;
+
+const
+  ResultText: array[TTestResult] of string = ('None', 'Passed', 'Failed');
+
+
 implementation
+
+uses
+  UContact;
+
+{ TTestCaseLoadSave }
+
+procedure TTestCaseLoadSave.Run;
+var
+  Lines: TStringList;
+begin
+  Lines := TStringList.Create;
+  try
+    with TContactsFile.Create do
+    try
+      Lines.Text := Input;
+      LoadFromStrings(Lines);
+      Lines.Text := '';
+      SaveToStrings(Lines);
+      Evaluate(Lines.Text = Output);
+      if Result <> trPassed then begin
+        Log := 'Expected:' + LineEnding +
+          '"' + Output + '"' + LineEnding + LineEnding +
+          'Output:' + LineEnding +
+          '"' + Lines.Text + '"';
+      end;
+    finally
+      Free;
+    end;
+  finally
+    Lines.Free;
+  end;
+end;
+
+procedure TTestCaseLoadSave.Evaluate(Passed: Boolean);
+begin
+  if Passed then Result := trPassed
+    else Result := trFailed;
+end;
 
 { TTestCase }
 
@@ -35,9 +90,9 @@ end;
 
 { TTestCases }
 
-function TTestCases.AddNew(Name: string): TTestCase;
+function TTestCases.AddNew(Name: string; TestClass: TTestCaseClass): TTestCase;
 begin
-  Result := TTestCase.Create;
+  Result := TestClass.Create;
   Result.Name := Name;
   Add(Result);
 end;
