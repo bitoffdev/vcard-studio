1b61d59ad7b11bbf130453eff913928031943f63
chronos <chronos@b2f690cb-8b89-471c-b043-dbd4632e1e77>
2022-02-04 10:30:24 +0000

    * Fixed: Quoted-printable encoded text was not handled correctly for multiple lines.

--- UQuotedPrintable.pas
+++ UQuotedPrintable.pas
@@ -7,285 +7,82 @@ interface
 uses
   Classes, SysUtils;
 
-function DecodeQuotedPrintable(Text: string): string;
-function EncodeQuotedPrintable(Text: string): string;
+function DecodeQuotedPrintable(Text: string; IgnoreErrors: Boolean = False): string;
+function EncodeQuotedPrintable(Text: string; EncodeSpaces: Boolean = False): string;
 
+const
+  QuotedPrintableEscapeCharacter = '=';
 
 implementation
 
+uses
+  UCommon;
+
 resourcestring
-  SLineLengthErr = 'Invalid line length for encoded text';
+  SDecodeError = 'Decode error';
 
-const
-  MaxLine = 1000;
-
-function DecodeQuotedPrintable(Text: string): string;
+function DecodeQuotedPrintable(Text: string; IgnoreErrors: Boolean = False): string;
 var
-  O, Count, WS: Integer;
-  I: integer;
-  InBuf: array[0..Pred(MaxLine)] of Byte;
-  OutBuf: array[0..Pred(MaxLine)] of Byte;
-  Decoding: Boolean;
-  Keeper: Boolean;
-  Abort: Boolean;
-  InStream: TMemoryStream;
-  OutStream: TMemoryStream;
+  I: Integer;
+  J: Integer;
+  C: Char;
+  IntValue: Integer;
 begin
   Result := '';
-  InStream := TMemoryStream.Create;
-  OutStream := TMemoryStream.Create;
-  try
-  if Text <> '' then begin
-    InStream.Write(Text[1], Length(Text));
-    InStream.Position := 0;
-  end;
-  Abort := False;
-  FillChar(InBuf, SizeOf(InBuf), #0);
-  WS := $FF;
-  Decoding := True;
-  Keeper := False;
-
-  { Skip any CR/LF's to get to the encoded stuff }
-  while True do begin
-    if InStream.Read(Char(InBuf[0]), 1) = 0then
-      Exit;
-    if ((InBuf[0] <> $0D) and (InBuf[0] <> $0A)) then begin
-      Keeper := True;
-      Break;
-    end;
-  end;
-
-  while Decoding and not Abort do begin
-    { Initialize }
-    if Keeper then begin
-      I := 1;
-      Keeper := False;
+  SetLength(Result, Length(Text));
+  I := 1;
+  J := 1;
+  while I <= Length(Text) do begin
+    C := Text[I];
+    if C = QuotedPrintableEscapeCharacter then begin
+      if ((I + 2) <= Length(Text)) and TryHexToInt(Text[I + 1] + Text[I + 2], IntValue) then begin
+        Result[J] := Chr(IntValue);
+        Inc(I, 2);
+        Inc(J);
+        SetLength(Result, Length(Result) - 2);
+      end else begin
+        if not IgnoreErrors then raise Exception.Create(SDecodeError)
+        else begin
+          Result[J] := '?';
+          Inc(J);
+        end;
+      end;
     end else begin
-      I := 0;
+      Result[J] := C;
+      Inc(J);
     end;
-    O := 0;
-
-    { Read in one line at a time - skipping over bad characters }
-    while True do begin
-      if (I > High(InBuf)) then
-        raise Exception.Create(SLineLengthErr);
-      if InStream.Read(Char(InBuf[I]), 1) = 0 then
-        Break;
-      case InBuf[I] of
-        $0A : Continue;
-        $0D : begin
-                Inc(I);
-                Break;
-              end;
-       { Test for potential end of data }
-       { '--' is probably the next Mime boundary }
-       { $2D : if (I = 1) and (InBuf[0] = $2D) then Exit;}
-      end;
-      Inc(I);
-    end;
-
-    if I = 0 then Break;
-    Count := I;
-    I := 0;
-
-    { Decode data to output stream }
-    while I < Count do begin
-      case InBuf[I] of
-        9       : begin
-                    if WS = $FF then
-                      WS := O;
-                    OutBuf[O] := InBuf[I];
-                    Inc(O);
-                    Inc(I);
-                  end;
-        13      : if WS = $FF then begin
-                    OutBuf[O] := 13;
-                    OutBuf[O+1] := 10;
-                    Inc(O, 2);
-                    Inc(I);
-                  end else begin
-                    OutBuf[WS] := 13;
-                    OutBuf[WS+1] := 10;
-                    O := WS+2;
-                    Inc(I);
-                  end;
-        32      : begin
-                    if WS = $FF then
-                      WS := O;
-                    OutBuf[O] := InBuf[I];
-                    Inc(O);
-                    Inc(I);
-                  end;
-        33..60  : begin
-                    WS := $FF;
-                    OutBuf[O] := InBuf[I];
-                    Inc(O);
-                    Inc(I);
-                  end;
-        61      : begin
-                    WS := $FF;
-                    if I+2 >= Count then Break;
-                    case InBuf[I+1] of
-                      48 : OutBuf[O] := 0;    {0}
-                      49 : OutBuf[O] := 16;   {1}
-                      50 : OutBuf[O] := 32;   {2}
-                      51 : OutBuf[O] := 48;   {3}
-                      52 : OutBuf[O] := 64;   {4}
-                      53 : OutBuf[O] := 80;   {5}
-                      54 : OutBuf[O] := 96;   {6}
-                      55 : OutBuf[O] := 112;  {7}
-                      56 : OutBuf[O] := 128;  {8}
-                      57 : OutBuf[O] := 144;  {9}
-                      65 : OutBuf[O] := 160;  {A}
-                      66 : OutBuf[O] := 176;  {B}
-                      67 : OutBuf[O] := 192;  {C}
-                      68 : OutBuf[O] := 208;  {D}
-                      69 : OutBuf[O] := 224;  {E}
-                      70 : OutBuf[O] := 240;  {F}
-                      97 : OutBuf[O] := 160;  {a}
-                      98 : OutBuf[O] := 176;  {b}
-                      99 : OutBuf[O] := 192;  {c}
-                     100 : OutBuf[O] := 208;  {d}
-                     101 : OutBuf[O] := 224;  {e}
-                     102 : OutBuf[O] := 240;  {f}
-                    end;
-                    case InBuf[I+2] of
-                      48 : ;                             {0}
-                      49 : OutBuf[O] := OutBuf[O] + 1;   {1}
-                      50 : OutBuf[O] := OutBuf[O] + 2;   {2}
-                      51 : OutBuf[O] := OutBuf[O] + 3;   {3}
-                      52 : OutBuf[O] := OutBuf[O] + 4;   {4}
-                      53 : OutBuf[O] := OutBuf[O] + 5;   {5}
-                      54 : OutBuf[O] := OutBuf[O] + 6;   {6}
-                      55 : OutBuf[O] := OutBuf[O] + 7;   {7}
-                      56 : OutBuf[O] := OutBuf[O] + 8;   {8}
-                      57 : OutBuf[O] := OutBuf[O] + 9;   {9}
-                      65 : OutBuf[O] := OutBuf[O] + 10;  {A}
-                      66 : OutBuf[O] := OutBuf[O] + 11;  {B}
-                      67 : OutBuf[O] := OutBuf[O] + 12;  {C}
-                      68 : OutBuf[O] := OutBuf[O] + 13;  {D}
-                      69 : OutBuf[O] := OutBuf[O] + 14;  {E}
-                      70 : OutBuf[O] := OutBuf[O] + 15;  {F}
-                      97 : OutBuf[O] := OutBuf[O] + 10;  {a}
-                      98 : OutBuf[O] := OutBuf[O] + 11;  {b}
-                      99 : OutBuf[O] := OutBuf[O] + 12;  {c}
-                     100 : OutBuf[O] := OutBuf[O] + 13;  {d}
-                     101 : OutBuf[O] := OutBuf[O] + 14;  {e}
-                     102 : OutBuf[O] := OutBuf[O] + 15;  {f}
-                    end;
-                    Inc(I, 3);
-                    Inc(O);
-                  end;
-        62..126 : begin
-                    WS := $FF;
-                    OutBuf[O] := InBuf[I];
-                    Inc(O);
-                    Inc(I);
-                  end;
-        else
-          Inc(I);
-      end;
-    end;
-
-    if O > 0 then
-      OutStream.Write(OutBuf, O)
-    else
-      Break;   { OutBuf is empty }
-  end;
-  SetLength(Result, OutStream.Size);
-  OutStream.Position := 0;
-  if OutStream.Size > 0 then
-    OutStream.Read(Result[1], Length(Result));
-  finally
-    OutStream.Free;
-    InStream.Free;
+    Inc(I);
   end;
 end;
 
-function EncodeQuotedPrintable(Text: string): string;
+function EncodeQuotedPrintable(Text: string; EncodeSpaces: Boolean): string;
 var
-  O, W: Integer;
-  WordBuf, OutBuf: array[0..80] of AnsiChar;
-  CurChar: AnsiChar;
-  Abort: Boolean;
-  InStream: TStream;
-  OutStream: TMemoryStream;
-
-  procedure SendLine;
-  begin
-    if (OutBuf[O - 1] = #9) or (OutBuf[O - 1] = #32) then begin
-      OutBuf[O] := '=';
-      Inc(O);
-    end;
-    OutStream.Write(OutBuf, O);
-    FillChar(OutBuf, SizeOf(OutBuf), #0);
-    O := 0;
-  end;
-
-  procedure AddWordToOutBuf;
-  var
-    J : Integer;
-  begin
-    if (O + W) > 74 then SendLine;
-    for J := 0 to (W - 1) do begin
-      OutBuf[O] := WordBuf[J];
-      Inc(O);
-    end;
-    W := 0;
-  end;
-
-  procedure AddHexToWord(B : Byte);
-  const
-    HexDigits : array[0..$F] of AnsiChar = '0123456789ABCDEF';
-  begin
-    if W > 73 then AddWordToOutBuf;
-    WordBuf[W] := '=';
-    WordBuf[W + 1] := HexDigits[B shr 4];
-    WordBuf[W + 2] := HexDigits[B and $F];
-    Inc(W, 3)
-  end;
-
+  I: Integer;
+  J: Integer;
+  C: Char;
+  LowerLimit: Char;
+const
+  HexDigits : array[0..$F] of AnsiChar = '0123456789ABCDEF';
 begin
+  if EncodeSpaces then LowerLimit := #33
+    else LowerLimit := #32;
   Result := '';
-  InStream := TMemoryStream.Create;
-  OutStream := TMemoryStream.Create;
-  try
-    if Text <> '' then begin
-      InStream.Write(Text[1], Length(Text));
-      InStream.Position := 0;
+  SetLength(Result, Length(Text));
+  I := 1;
+  J := 1;
+  while I <= Length(Text) do begin
+    C := Text[I];
+    if (C = QuotedPrintableEscapeCharacter) or (C < LowerLimit) or (C > #126) then begin
+      SetLength(Result, Length(Result) + 2);
+      Result[J] := QuotedPrintableEscapeCharacter;
+      Result[J + 1] := HexDigits[Ord(C) shr 4];
+      Result[J + 2] := HexDigits[Ord(C) and $f];
+      Inc(J, 3);
+    end else begin
+      Result[J] := C;
+      Inc(J);
     end;
-
-    Abort := False;
-    O := 0;
-    W := 0;
-    FillChar(OutBuf, SizeOf(OutBuf), #0);
-    while (InStream.Read(CurChar, 1) = 1) and not Abort do begin
-      if (Ord(CurChar) in [33..60, 62..126]) then begin
-        WordBuf[W] := CurChar;
-        Inc(W);
-        if W > 74 then AddWordToOutBuf;
-      end else if (CurChar = ' ') or (CurChar = #9) then begin
-        WordBuf[W] := CurChar;
-        Inc(W);
-        AddWordToOutBuf;
-      end else if (CurChar = #13) then begin
-        AddWordToOutBuf;
-        SendLine;
-      end else if (CurChar = #10) then begin
-        { Do nothing }
-      end else begin
-        AddHexToWord(Byte(CurChar));
-      end;
-    end;
-    AddWordToOutBuf;
-    OutStream.Write(OutBuf, O);
-    SetLength(Result, OutStream.Size);
-    OutStream.Position := 0;
-    if OutStream.Size > 0 then
-      OutStream.Read(Result[1], Length(Result));
-  finally
-    OutStream.Free;
-    InStream.Free;
+    Inc(I);
   end;
 end;
 
